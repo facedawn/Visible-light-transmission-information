76fba45becda5cc9b8f5a05884bd595de452e7c3
BeforeDawn <1597867839@qq.com>
2021-04-06 23:05:26 +0800

    整合文件

--- private/Eclosion/GenericGF.h
+++ private/Eclosion/GenericGF.h
@@ -2,31 +2,28 @@
 #ifndef GENERICGF_H
 #define GENERICGF_H
 
-#include<iostream>
-#include<cassert>
+#include<iostream>   
+#include<cassert>  
 #include <vector>
 #include <string>
 
-class GenericGF final
-{
+class GenericGF final {
 	private:
-	     std::vector<int> expTab;
-	     std::vector<int> logTab;
-	     int size;
-	     int primitive;
-	     int geneBase;
-
+		 std::vector<int> expTab;
+	   std::vector<int> logTab;
+	   int size;
+		 int primitive;
+	   int geneBase;
+  
 	public:
 		GenericGF() { size = 0, primitive = 0,geneBase=0; }
 		GenericGF(int primitive, int size, int b);
-		~GenericGF()
-		{
-			expTab.clear();
-			logTab.clear();
+		~GenericGF() {
+			expTab.clear(); expTab.swap(std::vector<int>());  logTab.clear(); logTab.swap(std::vector<int>());
 		};
-
+		
 		static int add_or_sub(int a, int b) {return a^b;}
-
+		 
 		int exp(const int a) { return expTab[a]; }
 
 		int log(const int a) { assert(!(a == 0)&&"Illegal Argument Exception!!!"); return logTab[a]; }
@@ -35,10 +32,14 @@ class GenericGF final
 
 		int multiply(const int a, const int b){ return (a == 0 || b == 0) ? 0 : expTab[(logTab[a] + logTab[b]) % (size - 1)]; }
 
-        int getSize(){ return size; }
+	  int getSize(){ return size; }
 
 		int getGeneBase(){ return geneBase; }
 
 		std::string to_String();
-};
-#endif
+	};
+
+	
+}
+
+#endif // !GENERICGF_H
